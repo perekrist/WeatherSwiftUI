869efa43e302613fc7b65172ceb58479832469ea
perekrist <perekrist31@gmail.com>
2020-02-12 00:34:58 +0700

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Перегудова Кристина on 12.02.2020.
+//  Copyright © 2020 perekrist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
